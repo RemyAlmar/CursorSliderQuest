b5d5f75119c065ebb2c2650183874b822b9f1fa9
Khenan Benanou <khenan.benanou@gmail.com>
2025-11-28 00:12:59 +0100

    [FEAT] Player DoActionButton: Click Feedback

--- Assets/Core/Player/DoActionButton.cs
+++ Assets/Core/Player/DoActionButton.cs
@@ -1,12 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoActionButton : MonoBehaviour, IClickable
 {
     [HideInInspector] public Player player;
 
+    Vector3 originalScale;
+    Coroutine clickFeedbackRoutine;
+
+    void Start()
+    {
+        originalScale = transform.localScale;
+    }
+
     public void OnClick()
     {
         player.DoAction();
+
+        if (clickFeedbackRoutine != null)
+        {
+            StopCoroutine(clickFeedbackRoutine);
+            transform.localScale = originalScale;
+        }
+        clickFeedbackRoutine = StartCoroutine(ClickFeedback());
     }
 
     public void OnClickOutside() { }
@@ -18,4 +34,31 @@ public class DoActionButton : MonoBehaviour, IClickable
     public void OnCursorExit() { }
 
     public void OnCursorUp() { }
+
+    IEnumerator ClickFeedback()
+    {
+        Vector3 originalScale = transform.localScale;
+        Vector3 targetScale = originalScale * 1.1f;
+        float duration = 0.1f;
+        float elapsed = 0f;
+
+        // Scale up
+        while (elapsed < duration)
+        {
+            transform.localScale = Vector3.Lerp(originalScale, targetScale, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = targetScale;
+
+        // Scale back down
+        elapsed = 0f;
+        while (elapsed < duration)
+        {
+            transform.localScale = Vector3.Lerp(targetScale, originalScale, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = originalScale;
+    }
 }
