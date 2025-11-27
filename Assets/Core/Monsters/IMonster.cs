e2dd11b744a5c0b963b989bba0cdbd3f737c91ce
Rémy <r.almar1997@gmail.com>
2025-11-27 23:24:38 +0100

    [FIX] Remove Interface

--- Assets/Core/Monsters/IMonster.cs
+++ Assets/Core/Monsters/IMonster.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public interface IEntity
 {
-    public int MaxHealth { get; }
-    public int CurrentHealth { get; }
     public void TakeDamage(int _damage);
     void Turn(IEntity _entity);
 }
--- Assets/Core/Scripts/Player/Player.cs
+++ Assets/Core/Scripts/Player/Player.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour, IEntity
 {
-    public int MaxHealth => throw new System.NotImplementedException();
-
-    public int CurrentHealth => throw new System.NotImplementedException();
-
     public void TakeDamage(int _damage)
     {
         throw new System.NotImplementedException();
