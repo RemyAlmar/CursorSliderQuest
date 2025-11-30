262daacc2beeb6a49313b139f7b98f1d487ce781
Khenan Benanou <khenan.benanou@gmail.com>
2025-11-30 12:14:19 +0100

    [FIX] Action name -> SlotAction

--- Assets/Core/Slots/SlotActions/SlotAction_Power.cs
+++ Assets/Core/Slots/SlotActions/SlotAction_Power.cs
@@ -1,29 +1,37 @@
 using System;
 
 [Serializable]
-public class Action_Void : Action
+public class SlotAction_Power : SlotAction
 {
+    public float powerMult = 1f;
     public override void ExecuteInTurn(IEntity executor, IEntity target, int indexSlot = -1)
     {
-        // Does nothing
+        
     }
     public override void ExecuteEndTurn(IEntity executor, IEntity target, int indexSlot = -1)
     {
-        // Does nothing
+        if (executor is Player _player)
+        {
+            Slot _slotRight = _player.GetNeighboringSlot(indexSlot, 1);
+            Slot _slotLeft = _player.GetNeighboringSlot(indexSlot, -1);
+
+            _slotRight?.MultiplyValue(powerMult);
+            _slotLeft?.MultiplyValue(powerMult);
+        }
     }
 
     public override void ResetTurn(IEntity executor, IEntity target, int indexSlot = -1)
     {
-        // Does nothing
+
     }
 
     public override void ResetFight(IEntity executor, IEntity target, int indexSlot = -1)
     {
-        // Does nothing
+
     }
 
     public override void MultiplyValue(float _multiplier)
     {
-        // Does nothing
+        powerMult *= _multiplier;
     }
 }
